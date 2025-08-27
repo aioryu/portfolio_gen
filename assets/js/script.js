a5ee501471768be60e03184e64d46fadd62dca78
Vitor Nazareth <vitorzath@gmail.com>
2025-08-26 21:00:26 -0300

    js fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,14 @@ const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 formulario.addEventListener("submit", function (event) {
   event.preventDefault();
 
+  let formValido = true;
+
   // Nome
   const campoNome = document.querySelector("#name");
   const errNome = document.querySelector("#errNome");
   if (campoNome.value.length < 3) {
     errNome.innerHTML = "O Nome deve ter no mínimo 3 caracteres.";
-    campoNome.focus();
-    return;
+    formValido = false;
   } else {
     errNome.innerHTML = "";
   }
@@ -20,8 +21,7 @@ formulario.addEventListener("submit", function (event) {
   const errEmail = document.querySelector("#errEmail");
   if (!campoEmail.value.match(emailRegex)) {
     errEmail.innerHTML = "Digite um E-mail válido. Ex: email@email.com";
-    campoEmail.focus();
-    return;
+    formValido = false;
   } else {
     errEmail.innerHTML = "";
   }
@@ -31,8 +31,7 @@ formulario.addEventListener("submit", function (event) {
   const errSubject = document.querySelector("#errSubject");
   if (campoSubject.value.length < 5) {
     errSubject.innerHTML = "O Assunto deve ter no mínimo 5 caracteres.";
-    campoSubject.focus();
-    return;
+    formValido = false;
   } else {
     errSubject.innerHTML = "";
   }
@@ -40,12 +39,13 @@ formulario.addEventListener("submit", function (event) {
   // Mensagem
   const campoMessage = document.querySelector("#message");
 
-  // Sucesso
-  window.alert("Obrigado pelo seu e-mail! Retornarei em contato em breve!");
-
-  // Limpa campos
-  campoNome.value = "";
-  campoEmail.value = "";
-  campoSubject.value = "";
-  campoMessage.value = "";
+  // Se o formulário for válido, mostra o alerta e limpa os campos
+  if (formValido) {
+    window.alert("Obrigado pelo seu e-mail! Retornarei em contato em breve!");
+    
+    campoNome.value = "";
+    campoEmail.value = "";
+    campoSubject.value = "";
+    campoMessage.value = "";
+  }
 });
